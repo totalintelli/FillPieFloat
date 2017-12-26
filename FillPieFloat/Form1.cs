13469e756947ef433a5a0d6c41115ddece26d95e
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-26 18:42:31 +0900

    first commit

---
+++ FillPieFloat/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FillPieFloat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void FillPieFloat(PaintEventArgs e)
+        {
+            // Create solid brush.
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+
+            // Create location and size of ellipse.
+            float x = 0.0F;
+            float y = 0.0F;
+            float width = 200.0F;
+            float height = 100.0F;
+
+            // Create start and sweep angles.
+            float startAngle = 0.0F;
+            float sweepAngle = 45.0F;
+
+            // Fill pie to screen.
+            e.Graphics.FillPie(redBrush, x, y, width, height, startAngle, sweepAngle);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            FillPieFloat(e);
+        }
+    }
+}
